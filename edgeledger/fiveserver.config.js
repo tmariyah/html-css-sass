655f53b8a40d1da96e8a6eb94136f6dca6a8d975
Mariya Tanveer <mariya.tanveer@invozone.com>
2024-04-02 16:16:37 +0500

    Contact functionality with PHP

---
+++ edgeledger/fiveserver.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  php: "/opt/homebrew/bin/php", //php executable
+  root: '/Users/mariyatanveer/Learning/2024/html-css-sass/edgeledger', //root directory of your project, where the liveserver looks
+  open: 'index.php', //entrypoint of your php project
+  injectBody: true //enable live reloading
+}
